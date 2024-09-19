ce8af65d673cd52bb5f994a41e50052abd1ec8e7
Jeisonbernardo <123920422+jeisontaboada@users.noreply.github.com>
2024-09-18 22:28:02 -0500

    Agregar archivos de proyecto.

---
+++ MVCCRUD/Models/PruebaEntradaContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+using MVCCRUD.Models;
+
+namespace MVCCRUD.Models;
+
+public partial class PruebaEntradaContext : DbContext
+{
+    public PruebaEntradaContext()
+    {
+    }
+
+    public PruebaEntradaContext(DbContextOptions<PruebaEntradaContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+          //  #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        // optionsBuilder.UseMySql("server=localhost;port=3306;database=pruebaEntrada;uid=root;password=fikmortvy02479bd", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.35-mysql"));
+        }
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+public DbSet<MVCCRUD.Models.Usuario> Usuario { get; set; } = default!;
+}
---
+++ MVCCRUD/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCCRUD.Models
+{
+    public partial class Usuario 
+    {
+       public int Id { get; set; }
+        public string? Nombre { get; set; }
+
+        public string? Apellido { get; set; }
+
+        public string? Dni { get; set; }
+    }
+}
